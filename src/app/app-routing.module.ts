79efbb3c1d7e4339e8525c702702a76e8f6f508a
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-06 13:30:45 +0100

    creación de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { ErrorComponent } from './error/error.component';
+import { NewComponent } from './new/new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/blog' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'new', component: NewComponent },
+  { path: '**', component: ErrorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
