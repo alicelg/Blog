2e75c316e6986811b60164025c4caa23cf6c8b20
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-06 13:39:31 +0100

    interface y metodos guia

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+export interface Post {
+  titulo: string,
+  texto: string,
+  autor: string,
+  imagen: string,
+  fecha: string,
+  categoria: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
   constructor() { }
+
+  agregarPost(Post) {
+
+  }
+
+  gettAllPosts() {
+
+  }
+
+  getPostsByCategoria(cat) {
+
+  }
 }
