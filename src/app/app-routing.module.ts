6137030608a8511cde4932694ad5504ded51a4a7
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-07 18:27:29 +0100

    añado ruta al detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
+import { DetallePostComponent } from './detalle-post/detalle-post.component';
 import { ErrorComponent } from './error/error.component';
 import { NewComponent } from './new/new.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/blog' },
   { path: 'blog', component: BlogComponent },
+  { path: 'blog/:postId', component: DetallePostComponent },
   { path: 'new', component: NewComponent },
   { path: '**', component: ErrorComponent },
 
