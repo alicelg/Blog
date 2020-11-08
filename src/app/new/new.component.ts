f7b68e32dd994ecf057736770112da5e191bf7b1
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-08 17:29:22 +0100

    validación del formulario

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormControlName, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../services/post.service';
 
 @Component({
@@ -14,13 +14,28 @@ export class NewComponent implements OnInit {
 
   constructor(private postService: PostService) {
     this.formPost = new FormGroup({
-      titulo: new FormControl(),
-      texto: new FormControl(),
-      autor: new FormControl(),
-      imagen: new FormControl(),
-      fecha: new FormControl(),
-      categoria: new FormControl(),
-      claves: new FormControl(),
+      titulo: new FormControl('', [
+        Validators.required
+      ]),
+      texto: new FormControl('', [
+        Validators.required,
+        Validators.minLength(150)
+      ]),
+      autor: new FormControl('', [
+        Validators.required,
+      ]),
+      imagen: new FormControl('', [
+        Validators.required,
+      ]),
+      fecha: new FormControl('', [
+        Validators.required,
+      ]),
+      categoria: new FormControl('', [
+        Validators.required,
+      ]),
+      claves: new FormControl('', [
+        Validators.required,
+      ]),
     })
     this.idPost = JSON.parse(localStorage.getItem('posts')) ? (JSON.parse(localStorage.getItem('posts')).slice(-1)[0].id + 1) : 1;
     /*     console.log(this.idPost);
